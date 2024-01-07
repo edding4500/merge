0ab78e0d84762f17684f17723e2dc21c4197a445
Tim Neumann <tin@stellwerk3.de>
2024-01-07 16:22:48 +0100

    Added documentation comments to Interval struct, constructor and Oberlaps function.

--- interval/interval.go
+++ interval/interval.go
@@ -2,11 +2,16 @@ package interval
 
 import "errors"
 
+//
+// Represents an interval with a start and end.
 type Interval struct {
 	Start int
 	End   int
 }
 
+//
+// Constructs a new interval with start and end.
+// If start > end, an error is returned.
 func New(start, end int) (*Interval, error) {
 	if start > end {
 		// throw error
@@ -16,6 +21,15 @@ func New(start, end int) (*Interval, error) {
 	}
 }
 
+// Returns true if the interval overlaps with the given interval.
+//
+// Example:
+// 	a, _ := interval.New(1, 3)
+// 	b, _ := interval.New(2, 4)
+// 	a.Overlaps(b) // true
+// 	a, _ := interval.New(1, 3)
+// 	b, _ := interval.New(4, 6)
+// 	a.Overlaps(b) // false
 func (i Interval) Overlaps(j Interval) bool {
 	return i.Start <= j.End && j.Start <= i.End
 }
